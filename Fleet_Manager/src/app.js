281d03182e93eedb891011e062a58a4f7e1457e9
tanvircse123 <tanvir.cse.cuet@gmail.com>
2023-05-11 21:32:02 +0600

    added

--- Fleet_Manager/src/app.js
+++ Fleet_Manager/src/app.js
@@ -2,8 +2,16 @@ import { Car } from "./classes/Car.js";
 import { Drone } from "./classes/Drone.js";
 import { fleet } from "./fleet-data.js";
 import { FleetDataService } from "./services/fleetDataService.js";
-import axios from '../node_modules/axios/dist/axios.js';
-let dataService = new FleetDataService();
-dataService.loadData(fleet);
-console.log(dataService.cars);
-console.log(dataService.drones);
+let url = "https://raw.githubusercontent.com/tanvircse123/ECMASCRIPT/main/Fleet_Manager/src/data.json";
+fetch(url)
+.then(res =>res.json()
+)
+.then(data=>{ 
+    let dataService = new FleetDataService();
+    dataService.loadData(data);
+    console.log(dataService.cars);
+    console.log(dataService.drones);
+})
+
+// let dataService = new FleetDataService();
+// dataService.loadData(fleet);
