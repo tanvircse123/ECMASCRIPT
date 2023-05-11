5b39030b6f6e2aa6906d2d0eac6077dd55a70fe5
tanvircse123 <tanvir.cse.cuet@gmail.com>
2023-05-11 21:02:10 +0600

    added

--- Fleet_Manager/src/app.js
+++ Fleet_Manager/src/app.js
@@ -2,9 +2,8 @@ import { Car } from "./classes/Car.js";
 import { Drone } from "./classes/Drone.js";
 import { fleet } from "./fleet-data.js";
 import { FleetDataService } from "./services/fleetDataService.js";
-
+import axios from '../node_modules/axios/dist/axios.js';
 let dataService = new FleetDataService();
 dataService.loadData(fleet);
-
-let cars = dataService.filterCarsByMake('U');
-console.log(cars);
+console.log(dataService.cars);
+console.log(dataService.drones);
